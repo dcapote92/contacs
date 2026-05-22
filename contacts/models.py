a0fbdec09409358a44279394379b415af1b44b54
dcapote92 <danicapote92@gmail.com>
2026-05-22 15:52:41 -0300

    add: contacts models

---
+++ contacts/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from ..database import Base
+
+
+class ContactModel(Base):
+    __tablename__ = "contacts"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+    )
+    name: Mapped[str]
+    email: Mapped[str]
+    phone: Mapped[str | None]
